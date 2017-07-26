316523a8acc13e88d0a714fd5c1618f45baeea31
rachaelcodes <rorourke.rsp@gmail.com>
2017-07-26 14:58:08 +0100

    adding notes to the files

---
+++ public/index.js
@@ -0,0 +1,3 @@
+//event listener to get the name -> make call to router.js
+
+//render DOM functions (2) renderComic() and renderCharacter()
---
+++ src/handlers.js
@@ -0,0 +1,6 @@
+//3 handler functions
+//loadAssets() for basic loading
+//api call function
+//getCharacter(name) calls API call using request module, call renderCharacter() and call getComic()
+  //3 responses: 200 and information in results, renderCharacter with results; 200 and empty results, renderCharacter with 'check your spelling'; other status codes renderCharacter 'sorry we're feeling sleepy - even superheroes need to rest'
+//getComic(id) calls API call using request module, call renderComic(): two cases 200 and not 200.
