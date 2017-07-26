63e085921804819b160f75f62bfe8cc398d47869
alinasolinas <azayneeva@gmail.com>
2017-07-27 00:56:53 +0100

    getCharacter function added, also some DOM manipulation added to index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,32 @@
 //event listener to get the name -> make call to router.js
 
 //render DOM functions (2) renderComic() and renderCharacter()
+
+  document.getElementById("searchForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var userInput = document.getElementById("characterInput").value;
+
+    const urlFrontend = "http://localhost:4000/" + "search/" + userInput;
+
+    getApi(urlFrontend, appendData)
+  })
+
+
+function getApi (url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var responseObj = JSON.parse(xhr.responseText);
+      callback(responseObj);
+    }
+  }
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+function appendData(responseObj) {
+  console.log("im response" + responseObj)
+  document.getElementById("results").textContent = responseObj[0].description
+  document.getElementById("results").style.display = 'block'
+}
--- src/urlBuilder.js
+++ src/urlBuilder.js
@@ -1,5 +1,8 @@
 //getCharacterURL capitalisation
 //getComicURL gets the ID from the previous call
 
-const env = require('env2')('./config.env');
+const env = require('env2')('.env');
 console.log(process.env.publicKey);
+
+
+//urlBuilder creates the url using the clean user input
