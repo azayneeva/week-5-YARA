88cdf28b1696254a70474e023ca1ddd610bd25ba
AseelM <aseelmustafa@outlook.com>
2017-07-26 18:03:26 +0100

    added event listener to index.js relates to #19

--- public/index.js
+++ public/index.js
@@ -1,3 +1,12 @@
 //event listener to get the name -> make call to router.js
 
 //render DOM functions (2) renderComic() and renderCharacter()
+
+var userInput = document.getElementById('searchForm');
+
+userInput.addEventListener('submit', function(event) {
+  var appendedInput = "/search/" + event.target.value;
+  console.log(appendedInput);
+  // loadAssets(appendedInput);
+  event.preventDefault();
+});
