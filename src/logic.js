4b29021cf9846694c999b2d1d9d317f2861d544c
Yahia <zaky20@hotmail.com>
2017-07-27 11:54:28 +0100

    made makeCharacter Url function relates #6 relates #24

---
+++ src/logic.js
@@ -0,0 +1,85 @@
+const handlers = require('./handlers');
+const request = require('./request');
+
+const env = require('env2')('.env');
+
+
+const API_URL = "https://gateway.marvel.com/v1/public"
+
+
+var logicObj = {
+
+  resultsObj: {},
+
+
+  makeCharacterUrl: function() {
+    const queryName = req.url.replace('/search/','')
+    const ts = Date.now()
+    const hash = crypto.createHash('md5').update(ts + process.env.PRIV_KEY + process.env.API_KEY).digest('hex')
+    const url = `${API_URL}/characters?name=${queryName}&apikey=${process.env.API_KEY}&ts=${ts}&hash=${hash}`
+    return url;
+  },
+
+  //change this
+  makeComicUrl: function() {
+    var urlStart = 'https://api.tfl.gov.uk/Line/';
+    var urlEnd = '/Status?app_id=de9e1a2e&app_key=41bcfcc2d033bae16403b619c8ec1613';
+    return urlStart + logicObj.resultsObj.line + urlEnd;
+  },
+
+  apiCall: function(url, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var data = JSON.parse(xhr.responseText);
+        cb(data);
+      }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+  },
+
+  characterProfile: function(tflData) {
+    var lineStatus = tflData[0].lineStatuses[0].statusSeverityDescription;
+    logicObj.resultsObj.status = lineStatus;
+  },
+
+  comicProfile: function(giphyData) {
+    var randomNum = Math.floor(Math.random()*30);
+    var gifSrc = giphyData.data[randomNum].images.fixed_height.url;
+    logicObj.resultsObj.url = gifSrc;
+  },
+
+  sendResponse(resultsObj){
+//write a function
+  };
+
+  waterfall: function(urlsArray,tasksArray, sendResponse) {
+      if (tasksArray.length > 0) {
+
+          var url = urlsArray[0]();
+          var remainingUrls = urlsArray.slice(1);
+          var task = tasksArray[0];
+          var remainingTasks = tasksArray.slice(1);
+
+
+          logicObj.apiCall(url, function(data) {
+              task(data);
+              logicObj.waterfall( remainingUrls,remainingTasks, sendResponse);
+          })
+
+      }
+      else {
+          logicObj.sendResponse(logicObj.resultsObj);
+      }
+  },
+
+  init: function(req, res) {
+
+      logicObj.waterfall( [logicObj.characterUrl, logicObj.comicUrl], [logicObj.getCharacter, logicObj.getComic], logicObj.sendResponse);
+  }
+
+}
+
+
+  module.exports=logicObj;
